c71f0bc124aff32ef55022a729e86288dc552dc3
shalvah <diakon.ng@gmail.com>
2020-03-29 01:22:58 +0100

    Implement basic status set

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
 
-exports.http = (request, response) => {
-  response.status(200).send('Hello World!');
-};
+const { WebClient } = require('@slack/web-api');
 
-exports.event = (event, callback) => {
-  callback();
+const token = process.env.SLACK_TOKEN;
+const web = new WebClient(token);
+
+exports.slashCommand = (request, response) => {
+  let time = req.body.text;
+
+  Promise.all([
+    web.users.profile.set({
+      profile: {
+        status_text: "Out to lunch",
+        status_emoji: ":yum:",
+        status_expiration: Math.round((Date.now() / 1000) + (1 * 60 * 60)),
+      }
+    })
+  ]).then(() => {
+    response.status(200).send('Hello World!');
+  });
 };
